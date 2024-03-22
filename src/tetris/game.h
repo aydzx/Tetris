845a14649a35e82eb71b4f25f274e9455dff3edd
Umana Dover <umanadov@ob-k4.nsk.21-school.ru>
2024-03-22 18:56:45 +0700

    collision

--- src/tetris/game.h
+++ src/tetris/game.h
@@ -1,21 +1,20 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include <string.h>
-#include <stdlib.h>
 #include <ncurses.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-
-/* colors */ 
-#define COLOR_BLACK	0
-#define COLOR_RED	1
-#define COLOR_GREEN	2
-#define COLOR_YELLOW	3
-#define COLOR_BLUE	4
-#define COLOR_MAGENTA	5
-#define COLOR_CYAN	6
-#define COLOR_WHITE	7
+/* colors */
+#define COLOR_BLACK 0
+#define COLOR_RED 1
+#define COLOR_GREEN 2
+#define COLOR_YELLOW 3
+#define COLOR_BLUE 4
+#define COLOR_MAGENTA 5
+#define COLOR_CYAN 6
+#define COLOR_WHITE 7
 
 WINDOW *gameWindow;
 char gameScreen[22][10];
--- src/tetris/main.c
+++ src/tetris/main.c
@@ -1,25 +1,24 @@
 #include "game.h"
 
 void initWindow() {
-    initscr();
-    cbreak();    
-    noecho();
-    keypad(stdscr, 1);
-    curs_set(0);
-    timeout(300); // speed
-    
-    use_default_colors();
-    start_color();
-    init_pair(1, COLOR_CYAN, -1);
-    init_pair(2, COLOR_YELLOW, -1);
-    init_pair(3, 203, -1);
-    init_pair(4, COLOR_BLUE, -1);
-    init_pair(5, COLOR_MAGENTA, -1);
-    init_pair(6, COLOR_GREEN, -1);
-    init_pair(7, COLOR_RED, -1);
+  initscr();
+  cbreak();
+  noecho();
+  keypad(stdscr, 1);
+  curs_set(0);
+  timeout(300);  // speed
 
-    
-    memset(gameScreen, 0, sizeof(gameScreen));
+  use_default_colors();
+  start_color();
+  init_pair(1, COLOR_CYAN, -1);
+  init_pair(2, COLOR_YELLOW, -1);
+  init_pair(3, 203, -1);
+  init_pair(4, COLOR_BLUE, -1);
+  init_pair(5, COLOR_MAGENTA, -1);
+  init_pair(6, COLOR_GREEN, -1);
+  init_pair(7, COLOR_RED, -1);
+
+  memset(gameScreen, 0, sizeof(gameScreen));
 }
 
 struct piece get_random_piece(void) {
@@ -39,138 +38,146 @@ struct piece get_random_piece(void) {
 int check_piece_overlap(struct piece current) {
   for (int y = 0; y < 4; y++) {
     for (int x = 0; x < 4; x++) {
-        if (tetris[current.piece][current.rotation][y][x] &&
-          (current.position.y - y > 12))
+      if (tetris[current.piece][current.rotation][y][x] && (
+        (current.position.y - y > 20) || (current.position.y - y < 0) ||
+        (current.position.x + x < 0) || (current.position.x + x >= 10)
+        // || (gameScreen[current.position.y - y][current.position.x + x])
+        )
+        )
         return 0;
     }
   }
   return 1;
 }
 
-
-
-
 int removeCurrentPiece(struct piece current) {
-    for (int y = 0; y < 4; ++y) {
-        for (int x = 0; x < 4; ++x) {
-            // if(gameScreen[current.position.y + y][current.position.x + x] == 1)
-            gameScreen[current.position.y - y][current.position.x + x] = 0;
-        }
+  for (int y = 0; y < 4; ++y) {
+    for (int x = 0; x < 4; ++x) {
+      if (tetris[current.piece][current.rotation][y][x])
+        gameScreen[current.position.y - y][current.position.x + x] = 0;
     }
+  }
 }
 
 int addCurrentPiece(struct piece current) {
-    for (int y = 0; y < 4; ++y) {
-        for (int x = 0; x < 4; ++x) {
-            if (tetris[current.piece][current.rotation][y][x])
-            gameScreen[current.position.y - y][current.position.x + x] = tetris[current.piece][current.rotation][y][x];
-        }
+  for (int y = 0; y < 4; ++y) {
+    for (int x = 0; x < 4; ++x) {
+      if (tetris[current.piece][current.rotation][y][x])
+        gameScreen[current.position.y - y][current.position.x + x] =
+            current.piece + 1;
     }
+  }
 }
 
 void printGameField(struct piece current) {
-    for (int y = 0; y < 22; y++) {
-      for (int x = 0; x < 10; x++) {
-        if (gameScreen[y][x]) {
-          wattron(gameWindow, COLOR_PAIR(gameScreen[y][x]));
-          mvwaddch(gameWindow, y + 1, x * 2 + 1, BLOCK);
-          mvwaddch(gameWindow, y + 1, x * 2 + 2, BLOCK);
-          wattroff(gameWindow, COLOR_PAIR(gameScreen[y][x]));
-        } else {
-          mvwaddch(gameWindow, y + 1, x * 2 + 1, ' ');
-          mvwaddch(gameWindow, y + 1, x * 2 + 2, ' ');
-        }
+  for (int y = 0; y < 22; y++) {
+    for (int x = 0; x < 10; x++) {
+      if (gameScreen[y][x]) {
+        wattron(gameWindow, COLOR_PAIR(gameScreen[y][x]));
+        mvwaddch(gameWindow, y + 1, x * 2 + 1, BLOCK);
+        mvwaddch(gameWindow, y + 1, x * 2 + 2, BLOCK);
+        wattroff(gameWindow, COLOR_PAIR(gameScreen[y][x]));
+      } else {
+        mvwaddch(gameWindow, y + 1, x * 2 + 1, ' ');
+        mvwaddch(gameWindow, y + 1, x * 2 + 2, ' ');
       }
     }
-    wrefresh(gameWindow);
+  }
+  wrefresh(gameWindow);
 }
 
 void moveLeft(struct piece* current) {
+  current->position.x -= 1;
+  if (check_piece_overlap(*current)) {
     removeCurrentPiece(*current);
-    current->position.x -= 1;
-    // gameScreen[current->position.y][current->position.x + 1] = 0;
     wrefresh(gameWindow);
+  } else {
+    current->position.x += 1;
+  }
 }
 
 void moveRight(struct piece* current) {
+  current->position.x += 1;
+  if (check_piece_overlap(*current)) {
     removeCurrentPiece(*current);
-    current->position.x += 1;
-    // gameScreen[current->position.y][current->position.x - 1] = 0;
     wrefresh(gameWindow);
+  } else {
+    current->position.x -= 1;
+  }
 }
 
 void moveDown(struct piece* current) {
-    //check
+  if (check_piece_overlap(*current)) {  // check
     removeCurrentPiece(*current);
     current->position.y += 1;
     // gameScreen[current->position.y - 1][current->position.x] = 0;
     wrefresh(gameWindow);
+  }
 }
 
 void rotate(struct piece* current) {
-    removeCurrentPiece(*current);
-    if(current->rotation == 3) {
-        current->rotation = 0;
-    } else {
+  if (current->rotation == 3) {
+    current->rotation = 0;
+  } else {
     current->rotation += 1;
-    }
+  }
+
+  if (check_piece_overlap(*current)) {
+    removeCurrentPiece(*current);
     wrefresh(gameWindow);
+  } else {
+    current->rotation -= 1;
+  }
 }
 
-int main(int argc, char **argv)
-{   
-    initWindow();
-    gameWindow = newwin(22 + 2, 10 * 2 + 2, 2, 10);
-    refresh();
+int main(int argc, char** argv) {
+  initWindow();
+  gameWindow = newwin(22 + 2, 10 * 2 + 2, 2, 10);
+  refresh();
 
-    struct piece current = get_random_piece();
-    
-    while (1) //
-    {   
-        wclear(gameWindow);
-        box(gameWindow, 0, 0);
-        switch (getch())
-        {
-            // case 'w':
-            //     current.position.y -= 1;
-            //     gameScreen[current.position.y + 1][current.position.x] = 0;
-            //     break;
-            case KEY_UP:
-            case 'w':
-                rotate(&current);
-                break;
-            case KEY_LEFT:
-            case 'a':
-                moveLeft(&current);
-                break;
-            case KEY_DOWN:
-            case 's':
-                moveDown(&current);
-                break;
-            case KEY_RIGHT:
-            case 'd':
-                moveRight(&current);
-                break;
-            case 'q':
-                return 0;
-                break;
-        }
+  struct piece current = get_random_piece();
+  struct piece next = get_random_piece();
 
-        addCurrentPiece(current);
+  while (1)  //
+  {
+    // wclear(gameWindow);
+    wborder(gameWindow, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK, BLOCK);
 
-        printGameField(current);
+    switch (getch()) {
+      case KEY_UP:
+      case 'w':
+        rotate(&current);
+        break;
+      case KEY_LEFT:
+      case 'a':
+        moveLeft(&current);
+        break;
+      case KEY_DOWN:
+      case 's':
+        moveDown(&current);
+        break;
+      case KEY_RIGHT:
+      case 'd':
+        moveRight(&current);
+        break;
+      case 'q':
+        return 0;
+        break;
+    }
 
-        //moveDown();
-        if(!check_piece_overlap(current)) {
-            moveDown(&current);
-        }// moveDown
+    addCurrentPiece(current);
 
-        wrefresh(gameWindow);
-    } //
+    printGameField(current);
 
+    moveDown(&current);
+    // if (!check_piece_overlap(current)) {
+    // current = next;
+    // next = get_random_piece();
+    // }
+    wrefresh(gameWindow);
+  }  //
 
-    delwin(gameWindow);
-    endwin();
-    return 0;
+  delwin(gameWindow);
+  endwin();
+  return 0;
 }
-
